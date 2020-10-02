9072c6c109994e2cf82c0d7f8d173bfe4a788758
Thomas Rebaud <thomasr@unito.io>
2020-10-02 00:18:13 -0400

    base

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Select from 'react-select';
+import Fretboard, { scaleNotes } from 'react-fretboard';
+import styled from 'styled-components';
+
+const Main = styled.main`
+	margin-top: 8rem;
+	margin-right: 4rem;
+`;
+
+const Settings = styled.div`
+	padding: 4rem;
+`;
+
+const Row = styled.div`
+	display: flex;
+`;
+
+const Col = styled.div`
+	flex: 50%;
+	padding: 1rem;
+	height: 16rem;
+`;
+
+const ROOT_NOTES = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'];
 
 function App() {
+	const [rootNote, setRootNote] = useState();
+
+	function getRootNoteOptions() {
+		return ROOT_NOTES.map(note => ({
+			label: note,
+			option: note,
+		}));
+	}
+
+	function getNeckNotes() {
+		const notes = scaleNotes(rootNote, 'major').map((n: any) => n.note);
+		return [
+			{ note: notes[0], status: 'root' },
+			{ note: notes[2], status: '3M' },
+			{ note: notes[4], status: '5P' },
+			{ note: notes[6], status: '7M' },
+		];
+	}
+
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
+		<Main>
+			<Fretboard
+				skinType="strings"
+				nrOfFrets={20}
+				showNotes
+				selectedNotes={getNeckNotes()}
+				theme={{
+					statusMap: {
+						'7M': '#b9e59e',
+						root: '#2196f3',
+						'3M': '#6ec6ff',
+						'5P': '#9a67ea',
+					},
+				}}
+			/>
+			<Settings>
+				<Row>
+					<Col style={{ backgroundColor: '#ede7f6' }}>
+						<Select
+							value={rootNote}
+							onChange={(option: any) => setRootNote(option.option)}
+							options={getRootNoteOptions()}
+						/>
+					</Col>
+					<Col style={{ backgroundColor: '#f7fcff' }}></Col>
+				</Row>
+			</Settings>
+		</Main>
 	);
 }
 
