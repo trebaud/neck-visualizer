a818fbfce1cc128fceb7bfb5ee3c4ec8cf793f3b
Thomas <thom.rebaud@hotmail.com>
2021-01-07 17:00:50 -0500

    Updates

---
+++ precache-manifest.20982ea1309d433b369f47533e9a00c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c006216deffe017a16d32a83686df30",
+    "url": "/neck-visualizer/index.html"
+  },
+  {
+    "revision": "e31ec4f8277215af2b17",
+    "url": "/neck-visualizer/static/css/main.c415fe82.chunk.css"
+  },
+  {
+    "revision": "684dac1d2b4ae7a71002",
+    "url": "/neck-visualizer/static/js/2.f23457fb.chunk.js"
+  },
+  {
+    "revision": "536558ae0d2c3676e46653cadf22447a",
+    "url": "/neck-visualizer/static/js/2.f23457fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e31ec4f8277215af2b17",
+    "url": "/neck-visualizer/static/js/main.a6c7c1d4.chunk.js"
+  },
+  {
+    "revision": "4e7597fef555c81af548",
+    "url": "/neck-visualizer/static/js/runtime-main.91a45543.js"
+  }
+]);
